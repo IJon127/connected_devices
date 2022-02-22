174179544d194114067f27dd7bb210f1045220ad
Ijon Hsieh <ih2168@nyu.edu>
2022-02-22 14:35:25 -0500

    debugging - loop

--- w4/arduino_BLE_accelerometer/arduino_BLE_accelerometer.ino
+++ w4/arduino_BLE_accelerometer/arduino_BLE_accelerometer.ino
@@ -3,7 +3,7 @@
   Uses Arduino LSM6DS3 library example (by Riccardo Rizzo): https://www.arduino.cc/en/Reference/ArduinoLSM6DS3
   and Tom Igoe's AS7341_Spectrometer_BLE: https://github.com/tigoe/LightProjects/blob/35061270d781354512571693dad21837ac4fc612/spectrometers/AS7341/AS7341_Spectrometer_BLE/AS7341_Spectrometer_BLE.ino
   created 20 Feb 2022
-  modified 20 Feb 2022
+  modified 21 Feb 2022
   by I-Jon Hsieh
  **************************************************************************/
 
@@ -14,30 +14,31 @@
   generate uuid for service in terminal by uuidgen
   replace 0000 with higher values (0001 ...)
 */
-char serviceUUID[] = "EB39FE3F-0000-45F1-9034-BB4FED33A133";  //[]????
-char characteristicUUID[] = "EB39FE3F-0001-45F1-9034-BB4FED33A133";
+//char serviceUUID[] = "EB39FE3F-0000-45F1-9034-BB4FED33A133";  //[]????
+//char characteristicUUID[] = "EB39FE3F-0001-45F1-9034-BB4FED33A133";
 
 //name the device
 char bleName[] = "accelerometer";
 const int readingLength = 17; //-0.00,-0.00,-0.00
 String xyz;
 
-BLEService acceleroService(serviceUUID);
+
 /*
   BLERead: ask the peripheral to send back the current value of the characteristic. Often used for characteristics that don't change very often, for example characteristics used for configuration, version numbers, etc.
   BLEWrite: modify the value of the characteristic. Often used for things that are like commands, for example telling the peripheral to turn a motor on or off.
   BLEIndicate and Notify: ask the peripheral to continuously send updated values of the characteristic, without the central having to constantly ask for it.
 */
-BLECharacteristic acceleroCharacteristic(characteristicUUID, BLERead | BLENotify,  readingLength); //Do we really need readingLength?
+BLEService acceleroService("EB39FE3F-0000-45F1-9034-BB4FED33A133");
+BLECharacteristic acceleroCharacteristic("EB39FE3F-0001-45F1-9034-BB4FED33A133", BLERead | BLENotify,  readingLength); //Do we really need readingLength?
 
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  //  while (!Serial);
 
   if (!IMU.begin()) {
     Serial.println("IMU failed");
-    while (1); //Why 1?
+    while (1); 
   }
   if (!BLE.begin()) {
     Serial.println("BLE failed");
@@ -45,7 +46,7 @@ void setup() {
   }
 
   xyz = "0.00,0.00,0.00";
-  
+
   BLE.setLocalName(bleName);
   BLE.setAdvertisedService(acceleroService);
   acceleroService.addCharacteristic(acceleroCharacteristic);
@@ -57,29 +58,28 @@ void setup() {
 }
 
 void loop() {
-  float x, y, z;
-
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(x, y, z);
-  }
-
-  String Sx = (String) x;
-  String Sy = (String) y;
-  String Sz = (String) z;
-  xyz = Sx + "," + Sy + "," + Sz;
-
-  
-  Serial.print("x: ");
-  Serial.print(x);
-  Serial.print(",  y: ");
-  Serial.print(y);
-  Serial.print(",  z: ");
-  Serial.println(z);
-  
-
   BLEDevice central = BLE.central(); //Does it have to be named as "central"?
   if (central) {
     while (central.connected()) {
+      float x, y, z;
+
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(x, y, z);
+      }
+
+      String Sx = (String) x;
+      String Sy = (String) y;
+      String Sz = (String) z;
+      xyz = Sx + "," + Sy + "," + Sz;
+
+
+      Serial.print("x: ");
+      Serial.print(x);
+      Serial.print(",  y: ");
+      Serial.print(y);
+      Serial.print(",  z: ");
+      Serial.println(z);
+
       acceleroCharacteristic.writeValue(xyz.c_str());
     }
   }
