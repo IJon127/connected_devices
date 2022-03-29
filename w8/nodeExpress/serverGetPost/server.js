05524e017bd42936e4af89f007b7af65fad30b03
Ijon Hsieh <ih2168@nyu.edu>
2022-03-29 19:20:43 -0400

    make two folders

---
+++ w8/nodeExpress/serverGetPost/server.js
@@ -0,0 +1,6 @@
+/**************************************************************************
+Node.js Express.js practice Uses Tom Igoe's express-basics.js:
+https://github.com/tigoe/NodeExamples/blob/main/ExpressIntro/express-basics.js
+created 29 March 2022 
+modified 29 March 2022 by I-Jon Hsieh
+**************************************************************************/
