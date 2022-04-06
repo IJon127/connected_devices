5955e38df8f86b65f009df6dbe65a715cd7ab75d
Ijon Hsieh <ih2168@nyu.edu>
2022-04-05 22:28:47 -0400

    create express server

--- w9/expressPractice/index.js
+++ w9/expressPractice/index.js
@@ -1,2 +1,23 @@
+//load module ------------
 const express = require("express");
+const fs = require("fs");
+
+//set server and port ------------
 const server = express();
+const port = 8080;
+
+//load pages & read files -------------
+const homePage = fs.readFileSync(`../public/index.html`, "utf-8");
+const data = fs.readFileSync(`../public/data.json`, "utf-8");
+
+//server ----------------------
+server.get("/", (req, res) => {
+  res.status(200).send(homePage);
+});
+server.get("/data", (req, res) => {
+  res.status(200).json(data);
+});
+
+server.listen(port, () => {
+  console.log("Listening to requests on port 8080...");
+});
