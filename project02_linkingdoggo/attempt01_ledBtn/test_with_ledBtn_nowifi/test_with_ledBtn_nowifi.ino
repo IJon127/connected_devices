a2040d2f78ac441252f773e823811144a2bad24d
Ijon Hsieh <ih2168@nyu.edu>
2022-04-19 17:22:18 -0400

    check both sensor values

--- project02_linkingdoggo/attempt01_ledBtn/test_with_ledBtn_nowifi/test_with_ledBtn_nowifi.ino
+++ project02_linkingdoggo/attempt01_ledBtn/test_with_ledBtn_nowifi/test_with_ledBtn_nowifi.ino
@@ -7,73 +7,58 @@
 const int greenLedPin = 2;         // the pin that the green LED is atteched to
 const int blueLedPin = 3;          // the pin that the blue LED is atteched to
 
-const int redBtnPin = 4;              // the pin that the red button is atteched to
-const int yellowBtnPin = 5;           // the pin that the yellow button is atteched to
+const int sensor1Pin = 4;              // the pin that the red button is atteched to
+const int sensor2Pin = 5;           // the pin that the yellow button is atteched to
 
-int state = LOW;             // by default, no motion detected
-int redBtnVal = 0;                 // variable to store the red button status (value)
-int yellowBtnVal = 0;                 // variable to store the yellow button status (value)
+int state1 = LOW;               // by default, no motion detected
+int state2 = LOW;               // by default, no human detected
+int val1 = LOW;                 // variable to store the red button status (value)
+int val2 = LOW;                 // variable to store the yellow button status (value)
 
-int dogIsMoving = 0;        // by default, no dog detected
-
-String subject;    // variable to store the subject being detected (human/dog)
 
 
 
 
 void setup() {
-  pinMode(greenLedPin, OUTPUT);      // initalize green LED as a digital output
-  pinMode(redBtnPin, INPUT);         // initialize red button as an input
-  pinMode(yellowBtnPin, INPUT);      // initialize yellow button as an input
-  Serial.begin(9600);                // initialize serial
+  pinMode(greenLedPin, OUTPUT);       // initalize green LED as a digital output
+  pinMode(sensor1Pin, INPUT);         // initialize red button as an input
+  pinMode(sensor2Pin, INPUT);         // initialize yellow button as an input
+  Serial.begin(9600);                 // initialize serial
 }
 
 void loop() {
-  redBtnVal = digitalRead(redBtnPin);   // read red button value
-  yellowBtnVal = digitalRead(yellowBtnPin);   // read yellow button value
+  val1 = digitalRead(sensor1Pin);   // read red button value
+  val2 = digitalRead(sensor2Pin);   // read yellow button value
 
-  if (redBtnVal == HIGH) {
-    if (yellowBtnVal == LOW) {
-      subject = "Dog";
+  state1 = checkSensorState(val1, greenLedPin, state1, "motion");
+  state2 = checkSensorState(val2, blueLedPin, state2, "human");
 
-      if (dogIsMoving == LOW) {
-        dogIsMoving = HIGH;
-        Serial.println("Moving Started! ");
-      }
-    } else {
-      subject = "Human";
-    }
-        Serial.print(subject);
-        Serial.println(" detected!");
-  } else {
-    dogIsMoving = LOW;
+
+  if(state1 == HIGH && state2 == LOW){
+    Serial.println("OPEN!!!");
   }
+}
 
 
 
-
-
-  /*
-    if (val == HIGH) {           // check if the sensor is HIGH
-    digitalWrite(greenLedPin, HIGH);   // turn LED ON
-    Serial.println("H!");
-    delay(100);                // delay 100 milliseconds
-
-
+int checkSensorState(int sensorVal, int ledPin, int state, String subject){
+    if (sensorVal == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(ledPin, HIGH);        // turn LED ON
+    delay(100);                        // delay 100 milliseconds
     if (state == LOW) {
-      Serial.println("Motion detected!");
-      state = HIGH;       // update variable state to HIGH
+      Serial.println(subject);
+      state = HIGH;                    // update variable state to HIGH
     }
+  }
+  else {
+    digitalWrite(ledPin, LOW);         // turn LED OFF
+    delay(200);                        // delay 200 milliseconds
+    if (state == HIGH) {
+      Serial.print("no ");
+      Serial.print(subject);
+      Serial.println(" is detected!");
+      state = LOW;                     // update variable state to LOW
     }
-    else {
-      digitalWrite(greenLedPin, LOW); // turn LED OFF
-      Serial.println("L!");
-      delay(500);             // delay 200 milliseconds
-
-      if (state == HIGH){
-        Serial.println("Motion stopped!");
-        state = LOW;       // update variable state to LOW
-    }
-    }
-  */
+  }
+  return state;
 }
